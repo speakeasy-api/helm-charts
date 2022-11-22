957312cef08650cca5b24c21863329b1a7f6bd34
Jose Pefaur <jose.pefaur@gmail.com>
2022-11-21 23:17:18 -0600

    use wilcard a record on aws

--- terraform/aws/dns.tf
+++ terraform/aws/dns.tf
@@ -12,11 +12,10 @@ resource "aws_route53_zone" "speakeasy_dns_zone" {
 
 data "aws_elb_hosted_zone_id" "main" {}
 
-resource "aws_route53_record" "grpc_dns_record" {
-  for_each = toset(local.grpcHostnames == null ? [] : local.grpcHostnames)
-  zone_id  = aws_route53_zone.speakeasy_dns_zone[0].zone_id
-  name     = each.value
-  type     = "A"
+resource "aws_route53_record" "wildcard_dns_record" {
+  zone_id = aws_route53_zone.speakeasy_dns_zone[0].zone_id
+  name    = "*.${var.domain}"
+  type    = "A"
 
   alias {
     name                   = module.speakeasy_k8s.nginx_loadbalancer_ip_or_hostname
@@ -25,11 +24,10 @@ resource "aws_route53_record" "grpc_dns_record" {
   }
 }
 
-resource "aws_route53_record" "api_dns_record" {
-  for_each = toset(local.apiHostnames == null ? [] : local.apiHostnames)
-  zone_id  = aws_route53_zone.speakeasy_dns_zone[0].zone_id
-  name     = each.value
-  type     = "A"
+resource "aws_route53_record" "domain_dns_record" {
+  zone_id = aws_route53_zone.speakeasy_dns_zone[0].zone_id
+  name    = var.domain
+  type    = "A"
 
   alias {
     name                   = module.speakeasy_k8s.nginx_loadbalancer_ip_or_hostname
@@ -38,28 +36,4 @@ resource "aws_route53_record" "api_dns_record" {
   }
 }
 
-resource "aws_route53_record" "web_dns_record" {
-  for_each = toset(local.webHostnames == null ? [] : local.webHostnames)
-  zone_id  = aws_route53_zone.speakeasy_dns_zone[0].zone_id
-  name     = each.value
-  type     = "A"
 
-  alias {
-    name                   = module.speakeasy_k8s.nginx_loadbalancer_ip_or_hostname
-    zone_id                = data.aws_elb_hosted_zone_id.main.id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "embed_dns_record" {
-  for_each = toset(local.embedFixtureHostnames == null ? [] : local.embedFixtureHostnames)
-  zone_id  = aws_route53_zone.speakeasy_dns_zone[0].zone_id
-  name     = each.value
-  type     = "A"
-
-  alias {
-    name                   = module.speakeasy_k8s.nginx_loadbalancer_ip_or_hostname
-    zone_id                = data.aws_elb_hosted_zone_id.main.id
-    evaluate_target_health = true
-  }
-}
