c4b3768712c01ca41a60eace5d54bf2da1ed123c
Jose Pefaur <jose.pefaur@gmail.com>
2022-11-21 22:23:19 -0600

    use wilcard a record on gcp

--- terraform/gcp/dns.tf
+++ terraform/gcp/dns.tf
@@ -11,44 +11,20 @@ resource "google_dns_managed_zone" "speakeasy_dns_zone" {
   visibility = var.domainZoneVisibility
 }
 
-resource "google_dns_record_set" "grpc_dns_record" {
-  for_each = toset(local.grpcHostnames == null ? [] : local.grpcHostnames)
-  name     = "${each.value}."
-  type     = "A"
-  ttl      = 300
+resource "google_dns_record_set" "wildcard_dns_record" {
+  name = "*.${var.domain}."
+  type = "A"
+  ttl  = 300
 
   managed_zone = google_dns_managed_zone.speakeasy_dns_zone[0].name
 
   rrdatas = [module.speakeasy_k8s.nginx_loadbalancer_ip_or_hostname]
 }
 
-resource "google_dns_record_set" "api_dns_record" {
-  for_each = toset(local.apiHostnames == null ? [] : local.apiHostnames)
-  name     = "${each.value}."
-  type     = "A"
-  ttl      = 300
-
-  managed_zone = google_dns_managed_zone.speakeasy_dns_zone[0].name
-
-  rrdatas = [module.speakeasy_k8s.nginx_loadbalancer_ip_or_hostname]
-}
-
-resource "google_dns_record_set" "web_dns_record" {
-  for_each = toset(local.webHostnames == null ? [] : local.webHostnames)
-  name     = "${each.value}."
-  type     = "A"
-  ttl      = 300
-
-  managed_zone = google_dns_managed_zone.speakeasy_dns_zone[0].name
-
-  rrdatas = [module.speakeasy_k8s.nginx_loadbalancer_ip_or_hostname]
-}
-
-resource "google_dns_record_set" "embed_dns_record" {
-  for_each = toset(local.embedFixtureHostnames == null ? [] : local.embedFixtureHostnames)
-  name     = "${each.value}."
-  type     = "A"
-  ttl      = 300
+resource "google_dns_record_set" "domain_dns_record" {
+  name = "${var.domain}."
+  type = "A"
+  ttl  = 300
 
   managed_zone = google_dns_managed_zone.speakeasy_dns_zone[0].name
 
