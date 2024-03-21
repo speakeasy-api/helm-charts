e6abb7a9d5bce22865fde806e273894696ac16c6
David Alberto Adler <dalberto.adler@gmail.com>
2024-03-21 10:37:53 +0000

    Update

--- terraform/aws/dns.tf
+++ terraform/aws/dns.tf
@@ -51,14 +51,3 @@ resource "aws_route53_record" "web_dns_record" {
   }
 }
 
-resource "aws_route53_record" "embed_dns_record" {
-  zone_id  = aws_route53_zone.speakeasy_dns_zone[0].zone_id
-  name     = each.value
-  type     = "A"
-
-  alias {
-    name                   = module.speakeasy_k8s.nginx_loadbalancer_ip_or_hostname
-    zone_id                = data.aws_elb_hosted_zone_id.main.id
-    evaluate_target_health = true
-  }
-}
